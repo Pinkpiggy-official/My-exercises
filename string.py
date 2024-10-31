8656693af0d793c5d4dca0163ee715684c7d0121
Pinkpiggy-official <mariya.serafyn@gmail.com>
2024-10-31 20:43:59 +0200

    string.py

---
+++ string.py
@@ -0,0 +1,23 @@
+	# конкатенація
+result = 'Hello ' + 'World'
+print(result)
+x = 'Hello ' + 'my ' + 'friend '
+print(x)
+
+
+	# довжина рядка
+length = len('Hello')
+print(length)
+z = input('How are you?')
+print(len(z))
+
+check_length = input('text: ')
+if len('text: ') > 10:
+	print('true')
+else:
+	print('false')
+	
+	
+	#індексація
+x = 'Python' [3]
+print(x)
